fd83a3263237489f82481d02c9c331a5e6fd7e74
bilofili <29082005fil@gmail.com>
2023-02-05 21:14:58 -0500

    fixed image

--- src/MyRecipeComponent.js
+++ src/MyRecipeComponent.js
@@ -4,16 +4,15 @@ function MyRecipeComponent({ caloriesProps, image, nameProps, ingredients}) {
     return(
         <div className="container">
             <h2>{nameProps}</h2>
-            <img src={image} />
+            <img src={image} alt="image_main" />
             <h3>Calories: {caloriesProps.toFixed()}</h3>
-
             <ul>
                 {ingredients.map(ingredient => (
-                    <li><img src={check} className="icon" alt="icon"/>{ingredient}</li>
+                    <li><img src={check} className="icon" alt="icons"/>{ingredient}</li>
                 ))};
             </ul>
         </div>
-    )
+    );
 }
 
 export default MyRecipeComponent;
